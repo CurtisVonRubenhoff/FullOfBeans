a20b3c81d751f547a6fcb1e99947a4e02ef59109
Curtis Cummings <me@curtiscummin.gs>
2024-01-27 22:57:51 -0600

    add mood action. wire beans to respond to mood events

--- Assets/BeanController.cs
+++ Assets/BeanController.cs
@@ -1,35 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BeanController : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     [ContextMenu("surprised")]
-
     public void BeSurprised()
     {
         _animator.Play("surprised");
     }
 
     [ContextMenu("blush")]
-
     public void BeBlushing()
     {
         _animator.Play("blushing");
-
     }
 
     [ContextMenu("Default")]
@@ -38,4 +35,19 @@ public class BeanController : MonoBehaviour
         _animator.Play("default");
     }
 
+    public void SetMood(string mood)
+    {
+        switch (mood)
+        {
+            case "surprised":
+                BeSurprised();
+                break;
+            case "blushing":
+                BeBlushing();
+                break;
+            default:
+                BeDefault();
+                break;
+        }
+    }
 }
---
+++ Assets/Scripts/Runtime/SetBeanMoodAction.cs
@@ -0,0 +1,39 @@
+﻿using CleverCrow.Fluid.Dialogues.Actions;
+using CleverCrow.Fluid.Dialogues;
+using ScriptableObjectArchitecture;
+
+
+namespace Runtime
+{
+
+    [CreateMenu("SetBeanMood")]
+    public class SetBeanMoodAction : ActionDataBase
+    {
+
+        public StringGameEvent moodChannel;
+        public string mood;
+        public override void OnInit (IDialogueController dialogue) {
+            // Run the first time the action is triggered
+        }
+
+        public override void OnStart () {
+            // Runs when the action begins triggering
+            moodChannel.Raise(mood);
+        }
+
+        public override ActionStatus OnUpdate () {
+            // Runs when the action begins triggering
+
+            // Return continue to span multiple frames
+            return ActionStatus.Success;
+        }
+
+        public override void OnExit () {
+            // Runs when the actions `OnUpdate()` returns `ActionStatus.Success`
+        }
+
+        public override void OnReset () {
+            // Runs after a node has fully run through the start, update, and exit cycle
+        }
+    }
+}
