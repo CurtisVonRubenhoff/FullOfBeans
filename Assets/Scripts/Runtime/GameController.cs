3e2227b37b1302eb0f86b2bdeec61a6b3c09645e
Curtis Cummings <me@curtiscummin.gs>
2024-01-27 18:53:12 -0600

    start rom structure

--- Assets/Scripts/Runtime/GameController.cs
+++ Assets/Scripts/Runtime/GameController.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using CleverCrow.Fluid.Dialogues;
+using CleverCrow.Fluid.Dialogues.Graphs;
 using Runtime;
 using UnityEngine;
 
+[System.Serializable]
+public class RoomData 
+{
+    public string RoomName;
+    public Sprite Background;
+    public DialogueGraph RoomDialogTree;
+    public List<ActorDefinition> Characters;
+}
+
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private List<RoomData> _rooms;
+
     // Start is called before the first frame update
     void Start()
     {
